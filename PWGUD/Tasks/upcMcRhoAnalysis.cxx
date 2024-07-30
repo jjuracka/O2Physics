70b17b76063e8573aab9014c5bab05eaea8586aa
Jakub Juracka <jakub.juracka@cern.ch>
2024-07-30 15:20:44 +0200

    working full prototype

--- PWGUD/Tasks/upcMcRhoAnalysis.cxx
+++ PWGUD/Tasks/upcMcRhoAnalysis.cxx
@@ -41,11 +41,8 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 
 struct upcMcRhoAnalysis {
-  // configurables
   double PcEtaCut = 0.9; // cut on track eta as per physics coordination recommendation
-  Configurable<bool> tracksRequireTOF{"tracksRequireTOF", false, "requireTOF"};
   Configurable<double> tracksTpcNSigmaPiCut{"tracksTpcNSigmaPiCut", 3.0, "tpcNSigmaPiCut"};
-//   Configurable<double> tracksTofNSigmaPiCut{"tracksTofNSigmaPiCut", 3.0, "tofNSigmaPiCut"};
   Configurable<double> tracksPtMaxCut{"tracksPtMaxCut", 1.0, "ptMaxCut"};
   Configurable<double> tracksDcaMaxCut{"tracksDcaMaxCut", 1.0, "dcaMaxCut"};
   Configurable<double> collisionsPosZMaxCut{"collisionsPosZMaxCut", 10.0, "posZMaxCut"};
@@ -53,16 +50,20 @@ struct upcMcRhoAnalysis {
   Configurable<double> ZNtimeCut{"ZNtimeCut", 2.0, "ZNtimeCut"};
   Configurable<double> systemMassMinCut{"2systemMassMinCut", 0.5, "2systemMassMinCut"};
   Configurable<double> systemMassMaxCut{"2systemMassMaxCut", 1.1, "2systemMassMaxCut"};
-  Configurable<double> systemPtCut{"2systemPtMaxCut", 0.12, "2systemPtMaxCut"};
+  Configurable<double> systemPtCut{"2systemPtMaxCut", 0.1, "2systemPtMaxCut"};
   Configurable<double> systemYCut{"2systemYCut", 0.9, "2systemYCut"};
 
-  ConfigurableAxis mAxis{"mAxis", {800, 0.0, 8.0}, "m (GeV/#it{c}^{2})"};
-  ConfigurableAxis ptAxis{"ptAxis", {500, 0.0, 5.0}, "#it{p}_{T} (GeV/#it{c})"};
-  ConfigurableAxis pt2Axis{"pt2Axis", {5000, 0.0, 0.05}, "#it{p}_{T}^{2} (GeV^{2}/#it{c}^{2})"};
+  ConfigurableAxis mAxis{"mAxis", {60, 0.5, 1.1}, "m (GeV/#it{c}^{2})"};
+  ConfigurableAxis ptAxis{"ptAxis", {10, 0.0, 0.1}, "#it{p}_{T} (GeV/#it{c})"};
+  ConfigurableAxis pt2Axis{"pt2Axis", {1000, 0.0, 0.1}, "#it{p}_{T}^{2} (GeV^{2}/#it{c}^{2})"};
   ConfigurableAxis etaAxis{"etaAxis", {180, -0.9, 0.9}, "#eta"};
   ConfigurableAxis yAxis{"yAxis", {180, -0.9, 0.9}, "y"};
-  ConfigurableAxis phiAxis{"phiAxis", {180, 0.0, 2.0*o2::constants::math::PI}, "#phi"};
+  ConfigurableAxis phiAxis{"phiAxis", {180, 0.0, 2.0 * o2::constants::math::PI}, "#phi"};
   ConfigurableAxis phiAssymAxis{"phiAssymAxis", {180, 0, o2::constants::math::PI}, "#phi"};
+  ConfigurableAxis nTracksAxis{"nTracksAxis", {101, -0.5, 100.5}, "N_{tracks}"};
+  ConfigurableAxis tpcNSigmaPiAxis{"tpcNSigmaPiAxis", {400, -10.0, 30.0}, "TPC n#sigma_{#pi}"};
+  ConfigurableAxis tofNSigmaPiAxis{"tofNSigmaPiAxis", {400, -20.0, 20.0}, "TOF n#sigma_{#pi}"};
+  ConfigurableAxis dcaAxis{"dcaXYAxis", {1000, -5.0, 5.0}, "DCA (cm)"};
 
   HistogramRegistry registry{"registry", {}, OutputObjHandlingPolicy::AnalysisObject};
 
@@ -81,48 +82,64 @@ struct upcMcRhoAnalysis {
   }
 
   template <typename T>
-  bool trackPassesCuts(T const& track) { // check if track passes preliminary cuts (PID done separately)
-    if (!track.isPVContributor()) return false;
-    if (!track.hasITS()) return false;
-    if (std::abs(track.dcaZ()) > tracksDcaMaxCut || std::abs(track.dcaXY()) > tracksDcaMaxCut) return false;
-    if (std::abs(eta(track.px(), track.py(), track.pz())) > PcEtaCut) return false;
-    if (std::abs(track.pt()) > tracksPtMaxCut) return false;
+  bool trackPassesCuts(T const& track) // track passes preliminary cuts (PID done separately)
+  {
+    if (!track.isPVContributor())
+      return false;
+    if (!track.hasITS())
+      return false;
+    if (std::abs(track.dcaZ()) > tracksDcaMaxCut || std::abs(track.dcaXY()) > tracksDcaMaxCut)
+      return false;
+    if (std::abs(eta(track.px(), track.py(), track.pz())) > PcEtaCut)
+      return false;
+    if (std::abs(track.pt()) > tracksPtMaxCut)
+      return false;
     return true;
   }
 
   template <typename T>
-  bool tracksPassPiPID(const T& cutTracks) { // check if pre-cut tracks pass PID cut (fall within n-dimensional hypersphere)
+  bool tracksPassPiPID(const T& cutTracks) // pre-cut tracks pass n-dim PID cut
+  {
     double radius = 0.0;
-    for (const auto& track : cutTracks) radius += std::pow(track.tpcNSigmaPi(), 2);
+    for (const auto& track : cutTracks)
+      radius += std::pow(track.tpcNSigmaPi(), 2);
     return radius < std::pow(tracksTpcNSigmaPiCut, 2);
   }
 
   template <typename T>
-  double tracksTotalCharge(const T& cutTracks) { // calculate total charge of selected tracks
+  double tracksTotalCharge(const T& cutTracks) // total charge of selected tracks
+  {
     double charge = 0.0;
-    for (const auto& track : cutTracks) charge += track.sign();
+    for (const auto& track : cutTracks)
+      charge += track.sign();
     return charge;
   }
 
   template <typename T>
-  bool systemPassCuts(const T& system) { // check if system passes system cuts
-    if (system.M() < systemMassMinCut || system.M() > systemMassMaxCut) return false;
-    if (system.Pt() > systemPtCut) return false;
-    if (std::abs(system.Rapidity()) > systemYCut) return false;
+  bool systemPassCuts(const T& system) // reco system passes system cuts
+  {
+    if (system.M() < systemMassMinCut || system.M() > systemMassMaxCut)
+      return false;
+    if (system.Pt() > systemPtCut)
+      return false;
+    if (std::abs(system.Rapidity()) > systemYCut)
+      return false;
     return true;
   }
 
-  ROOT::Math::PxPyPzMVector reconstructSystem(const std::vector<ROOT::Math::PxPyPzMVector>& cutTracks4Vecs) { // reconstruct system from 4-vectors
+  ROOT::Math::PxPyPzMVector reconstructSystem(const std::vector<ROOT::Math::PxPyPzMVector>& cutTracks4Vecs) // reconstruct system from 4-vectors
+  {
     ROOT::Math::PxPyPzMVector system;
-    for (const auto& track4Vec : cutTracks4Vecs) system += track4Vec;
+    for (const auto& track4Vec : cutTracks4Vecs)
+      system += track4Vec;
     return system;
   }
 
   template <typename T>
-  double getPhiAssym(const T& cutTracks) { // phi anisotropy based on STAR + ALICE papers
-    // assign the tracks randomly
+  double getPhiRandom(const T& cutTracks) // decay phi anisotropy
+  {                                       // two possible definitions of phi: randomize the tracks
     std::vector<int> indices = {0, 1};
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); // get time-based seed
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();    // get time-based seed
     std::shuffle(indices.begin(), indices.end(), std::default_random_engine(seed)); // shuffle indices
     // calculate phi
     ROOT::Math::XYVector pOne(cutTracks[indices[0]].px(), cutTracks[indices[0]].py());
@@ -130,11 +147,29 @@ struct upcMcRhoAnalysis {
     auto pPlus = pOne + pTwo;
     auto pMinus = pOne - pTwo;
     // no method for direct calculation of angle -> use dot product formula
-    double cosPhi = (pPlus.Dot(pMinus))/(std::sqrt(pPlus.Mag2()) * std::sqrt(pMinus.Mag2()));
+    double cosPhi = (pPlus.Dot(pMinus)) / (std::sqrt(pPlus.Mag2()) * std::sqrt(pMinus.Mag2()));
     return std::acos(cosPhi);
   }
 
-  void processSim(aod::McCollision const& mccollision, aod::McParticles const& mcparticles) {
+  template <typename T>
+  double getPhiCharge(const T& cutTracks)
+  { // two possible definitions of phi: charge-based assignment
+    ROOT::Math::XYVector pOne, pTwo;
+    if (cutTracks[0].sign() > 0) {
+      pOne.SetXY(cutTracks[0].px(), cutTracks[0].py());
+      pTwo.SetXY(cutTracks[1].px(), cutTracks[1].py());
+    } else {
+      pOne.SetXY(cutTracks[1].px(), cutTracks[1].py());
+      pTwo.SetXY(cutTracks[0].px(), cutTracks[0].py());
+    }
+    // calculate phi
+    auto pPlus = pOne + pTwo;
+    auto pMinus = pOne - pTwo;
+    double cosPhi = (pPlus.Dot(pMinus)) / (std::sqrt(pPlus.Mag2()) * std::sqrt(pMinus.Mag2()));
+    return std::acos(cosPhi);
+  }
+
+  void processSim(aod::McCollision const& /* mccollision */, aod::McParticles const& mcparticles) {
     std::vector<ROOT::Math::PxPyPzMVector> mcPions;
     for (const auto& mcparticle : mcparticles) {
         registry.fill(HIST("QC/hMcParticlePdgCode"), mcparticle.pdgCode());
@@ -152,16 +187,28 @@ struct upcMcRhoAnalysis {
     registry.fill(HIST("sim/hRhoPt"), rho.Pt());
     registry.fill(HIST("sim/hRhoY"), rho.Rapidity());
     registry.fill(HIST("sim/hRhoPhi"), rho.Phi() + o2::constants::math::PI);
-  } PROCESS_SWITCH(upcMcRhoAnalysis, processSim, "analyse simulated tracks", true);
+  } 
+  PROCESS_SWITCH(upcMcRhoAnalysis, processSim, "analyse simulated tracks", true);
 
-//   void processReco(aod::Collision const& collision, soa::Join<aod::Tracks, aod::pidTPCPi, aod::TracksExtra, aod::TracksDCA, aod::TrackSelection> const& tracks) {
-//     if (std::abs(collision.posZ()) > collisionsPosZMaxCut) return;
-//     std::vector<decltype(tracks.begin())> cutTracks;
-//     for (const auto& track : tracks) {
-//       if (!trackPassesCuts(track)) continue;
-//       cutTracks.push_back(track);
-//     }
-//   } PROCESS_SWITCH(upcMcRhoAnalysis, processReco, "analyse reconstructed tracks", true);
+  void processReco(aod::Collision const& collision, soa::Join<aod::Tracks, aod::pidTPCPi, aod::TracksExtra, aod::TracksDCA, aod::TrackSelection> const& tracks) {
+    if (std::abs(collision.posZ()) > collisionsPosZMaxCut) return;
+    std::vector<decltype(tracks.begin())> cutTracks;
+    std::vector<ROOT::Math::PxPyPzMVector> recoPions;
+    for (const auto& track : tracks) {
+      if (!trackPassesCuts(track)) continue;
+      cutTracks.push_back(track);
+      recoPions.push_back(ROOT::Math::PxPyPzMVector(track.px(), track.py(), track.pz(), o2::constants::physics::MassPionCharged));
+    }
+    if (!tracksPassPiPID(cutTracks)) return;
+    if (cutTracks.size() != 2) return;
+    auto rho = reconstructSystem(recoPions);
+    if (!systemPassCuts(rho)) return;
+    registry.fill(HIST("reco/hRhoMass"), rho.M());
+    registry.fill(HIST("reco/hRhoPt"), rho.Pt());
+    registry.fill(HIST("reco/hRhoY"), rho.Rapidity());
+    registry.fill(HIST("reco/hRhoPhi"), rho.Phi() + o2::constants::math::PI);
+  }
+  PROCESS_SWITCH(upcMcRhoAnalysis, processReco, "analyse reconstructed tracks", true);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc) {
